c9f1e6720c1b0eee24ffba43cf654527c4152df5
Vinicius Grippa <viniciusdegoesgrippa@outlook.com>
2021-06-08 08:42:55 -0300

    Adicionado um Enum com os tipos de veiculos

---
+++ Enum/TipoVeiculo.cs
@@ -0,0 +1,17 @@
+namespace Sistema.Estacionamento
+{
+    public enum TipoVeiculo
+    {
+        Passeio = 1,
+
+        Utilitario = 2,
+
+        Pickup = 3,
+
+        SUV = 4,
+
+        Caminhao = 5,
+
+        Onibus = 6
+    }
+}
